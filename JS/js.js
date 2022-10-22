f6ab0e200ac2ecd2b56fdb8b4f3f267d1d0b6563
abdulaziz2727 <116353029+abdulaziz2727@users.noreply.github.com>
2022-10-22 16:49:17 +0500

    Lesson
    Lesson

---
+++ JS/js.js
@@ -0,0 +1,11 @@
+var a =0; //o'zgaruvchi ochadugan kalit so'zlar[ let, war, const]
+let b =0;
+a= 20;
+const c =0;
+//vall
+let ab ="abcd1234"//data type string
+let ad = 123 // data type number 
+let un; //data type undefined
+lwt nu =null; //data type null
+nu = data; 
+
