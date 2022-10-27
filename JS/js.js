57c8743fcec26543ec2988c110b3a118d3a21e2c
abdulaziz2727 <abdulazizaslanov01@gmail.com>
2022-10-27 13:56:44 +0000

    corusel started

--- JS/js.js
+++ JS/js.js
@@ -1,2 +1,18 @@
+let list =document.querySelector("#list");
+let button =document.querySelector("#btn");
+let left_button =document.querySelector("left_button");
+let right_button =document.querySelector("right_button");
+let img = document.querySelector("corusel_item");
+
+
+button.addEventListener("click" , ()=>{
+    if(list.classList[1]=="toggle_list"){
+        list.classList.remove("toggle_list")
+    }else{
+        list.classList.add("toggle_list")
+    }
+    console.log(list.classList);
+})
+
 
 
